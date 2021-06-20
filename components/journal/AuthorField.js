4b5db0faeae691037e592b4021926d1873823500
Eric Yang Jia Wei <eric.yang@temasek.nus.edu.sg>
2021-06-21 00:50:53 +0800

    Refactor Author Field

--- components/journal/AuthorField.js
+++ components/journal/AuthorField.js
@@ -2,46 +2,52 @@ import React from "react";
 import { Field } from "react-final-form";
 import { Grid, TextField } from "@material-ui/core";
 
-function AuthorField() {
+const AuthorField = () => {
   return (
     <Grid
       container
       style={{ justifyContent: "space-between", marginBottom: 20 }}
     >
       <Grid item>
-        <Field
-          style={{ width: 300 }}
-          required
-          fullWidth
-          name="firstName"
-          component={TextField}
-          type="text"
-          placeholder="First Name"
-        />
+        <Field name="firstName" type="text">
+          {({ input }) => (
+            <TextField
+              style={{ width: 300 }}
+              required
+              fullWidth
+              placeholder="First Name"
+              inputProps={input}
+            />
+          )}
+        </Field>
       </Grid>
       <Grid item>
-        <Field
-          style={{ width: 300 }}
-          fullWidth
-          name="middleNametle"
-          component={TextField}
-          type="text"
-          placeholder="Middle Name"
-        />
+        <Field name="middleName" type="text">
+          {({ input }) => (
+            <TextField
+              style={{ width: 300 }}
+              fullWidth
+              placeholder="Middle Name"
+              inputProps={input}
+            />
+          )}
+        </Field>
       </Grid>
       <Grid>
-        <Field
-          style={{ width: 300 }}
-          required
-          fullWidth
-          name="lastName"
-          component={TextField}
-          type="text"
-          placeholder="Last Name"
-        />
+        <Field name="lastName" type="text">
+          {({ input }) => (
+            <TextField
+              style={{ width: 300 }}
+              required
+              fullWidth
+              placeholder="Last Name"
+              inputProps={input}
+            />
+          )}
+        </Field>
       </Grid>
     </Grid>
   );
-}
+};
 
 export default AuthorField;
